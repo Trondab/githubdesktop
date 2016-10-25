23e24bbe6ea48739592bbfa7f8a397d4c88cc619
Trondab <tabolset@hotmail.com>
2016-10-25 10:25:06 +0200

    made a class in java.
    new class named "Github"

---
+++ src/githubdesktop/Githubdesktop.java
@@ -0,0 +1,17 @@
+
+package githubdesktop;
+
+/**
+ *
+ * @author TrondArild
+ */
+public class Githubdesktop {
+
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+    
+}
+class Github{
+    private int number;
+}
